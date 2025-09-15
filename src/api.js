1fc8b47ff6d8f448340c1678c0f4a1da7948c139
savickuse <savickuse@users.noreply.github.com>
2025-09-15 09:27:10 +0300

    improve(tests): fix security vulnerability

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 27
 module.exports = API;
+
+
+// API module for SystemCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 38
+module.exports = API;
