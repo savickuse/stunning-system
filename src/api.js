d80599f048bd9c61c1c3af40eff39c973e52e1d7
savickuse <savickuse@users.noreply.github.com>
2025-09-19 23:08:10 +0300

    perf: fix async handling

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 54
 module.exports = API;
+
+
+// API module for SystemCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 70
+module.exports = API;
