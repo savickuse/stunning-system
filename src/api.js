dba41521288b0b034437315b19d66731921a6fd1
savickuse <savickuse@users.noreply.github.com>
2025-10-09 19:51:48 +0300

    ci: fix race condition

--- src/api.js
+++ src/api.js
@@ -23,3 +23,30 @@ class API {
 
 // Update 11
 module.exports = API;
+
+
+// API module for SystemCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 20
+module.exports = API;
