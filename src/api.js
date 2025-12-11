256900b4c0fe7bec7e782880fac08f8b666e2d08
savickuse <savickuse@users.noreply.github.com>
2025-12-11 21:08:50 +0200

    remove: bug fix for edge case

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 51
 module.exports = API;
+
+
+// API module for SystemCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 54
+module.exports = API;
