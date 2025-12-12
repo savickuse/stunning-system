c9372e0d2ad1462521080086c62814b3e5f5bfcc
savickuse <savickuse@users.noreply.github.com>
2025-12-12 20:13:07 +0200

    fix: optimize rendering

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 38
 module.exports = API;
+
+
+// API module for SystemCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 51
+module.exports = API;
